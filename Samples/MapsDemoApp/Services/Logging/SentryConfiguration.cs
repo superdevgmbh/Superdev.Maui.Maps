6cc3607495a25ee04dfb8e27c4d9185b59f4a626
thomasgalliker <thomas@bluewin.ch>
2025-10-23 20:01:04 +0200

    Update sentry DSN

--- Samples/MapsDemoApp/Services/Logging/SentryConfiguration.cs
+++ Samples/MapsDemoApp/Services/Logging/SentryConfiguration.cs
@@ -10,7 +10,7 @@ namespace MapsDemoApp.Services.Logging
         {
             options.InitializeSdk = true;
             options.Debug = false;
-            //options.Dsn = "TBD";
+            options.Dsn = "https://6a656feef8c47b176c0faf814a79ff85@o4507458300280832.ingest.de.sentry.io/4510240104448080";
             options.MinimumEventLevel = LogLevel.Warning;
             options.MinimumBreadcrumbLevel = LogLevel.Debug;
         }
